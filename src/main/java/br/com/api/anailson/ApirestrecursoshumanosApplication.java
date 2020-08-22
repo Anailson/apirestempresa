e40137f4b386b273159832065ad6ee02190c75f1
Anailson Ribeiro <arsantos315@gmail.com>
2020-08-22 16:16:19 -0300

    Projeto Criado na Ide

---
+++ src/main/java/br/com/api/anailson/ApirestrecursoshumanosApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.anailson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestrecursoshumanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestrecursoshumanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/anailson/ApirestrecursoshumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.anailson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestrecursoshumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
